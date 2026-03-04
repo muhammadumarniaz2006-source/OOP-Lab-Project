17d3b8900e83998dd86845ef0bf05f808bb798bd
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2026-03-04 23:49:01 +0500

    Formatted and changed some functions names

--- lib/classes.hpp
+++ lib/classes.hpp
@@ -3,75 +3,67 @@
 #include <cmath>
 
 using namespace std;
+
 class Piece;
+
 enum class Color { WHITE, BLACK };
-typedef struct
-{
+
+typedef struct {
     int x;
     int y;
 } Pos;
-class Board
-{
+
+class Board{
 public:
     Piece *squares[8][8];
     Color color;
-    Board()
-{
-  
-    for (int r = 0; r < 8; r++)
-    {
-        for (int c = 0; c < 8; c++)
+    Board(){
+        for (int r = 0; r < 8; r++)
         {
-            squares[r][c] = nullptr;
+            for (int c = 0; c < 8; c++)
+            {
+                squares[r][c] = nullptr;
+            }
+        }
+    }
+    ~Board(){
+        for (int r = 0; r < 8; r++){
+            for (int c = 0; c < 8; c++){
+                delete squares[r][c];
+                squares[r][c] = nullptr;
+            }
         }
     }
 
-}
-~Board()
-{
-    for (int r = 0; r < 8; r++)
-    {
-        for (int c = 0; c < 8; c++)
-        {
-            delete squares[r][c];
-            squares[r][c] = nullptr;
+    void initBoard(){
+        for (int c = 0; c < 8; c++){
+            squares[1][c] = new Pawn(Color::BLACK);
+            squares[6][c] = new Pawn(Color::WHITE);
         }
+        squares[0][0] = new Rook(Color::BLACK);
+        squares[0][7] = new Rook(Color::BLACK);
+        squares[7][0] = new Rook(Color::WHITE);
+        squares[7][7] = new Rook(Color::WHITE);
+        squares[0][1] = new Knight(Color::BLACK);
+        squares[0][6] = new Knight(Color::BLACK);
+        squares[7][1] = new Knight(Color::WHITE);
+        squares[7][6] = new Knight(Color::WHITE);
+        squares[0][2] = new Bishop(Color::BLACK);
+        squares[0][5] = new Bishop(Color::BLACK);
+        squares[7][2] = new Bishop(Color::WHITE);
+        squares[7][5] = new Bishop(Color::WHITE);
+        squares[0][3] = new Queen(Color::BLACK);
+        squares[7][3] = new Queen(Color::WHITE);
+        squares[0][4] = new King(Color::BLACK);
+        squares[7][4] = new King(Color::WHITE);
     }
-}
-void initializeBoard()
-{
-   
-    for (int c = 0; c < 8; c++)
-    {
-        squares[1][c] = new Pawn(Color::BLACK);
-        squares[6][c] = new Pawn(Color::WHITE);
-    }
-    squares[0][0] = new Rook(Color::BLACK);
-    squares[0][7] = new Rook(Color::BLACK);
-    squares[7][0] = new Rook(Color::WHITE);
-    squares[7][7] = new Rook(Color::WHITE);
-    squares[0][1] = new Knight(Color::BLACK);
-    squares[0][6] = new Knight(Color::BLACK);
-    squares[7][1] = new Knight(Color::WHITE);
-    squares[7][6] = new Knight(Color::WHITE);
-    squares[0][2] = new Bishop(Color::BLACK);
-    squares[0][5] = new Bishop(Color::BLACK);
-    squares[7][2] = new Bishop(Color::WHITE);
-    squares[7][5] = new Bishop(Color::WHITE);
-    squares[0][3] = new Queen(Color::BLACK);
-    squares[7][3] = new Queen(Color::WHITE);
-    squares[0][4] = new King(Color::BLACK);
-    squares[7][4] = new King(Color::WHITE);
-}
-    bool isempty(int row, int col)
-    {
 
-        if (row < 0 || row >= 8 || col < 0 || col >= 8)
-            return false;
-
+    bool isEmpty(int row, int col){
+        if (row < 0 || row >= 8 || col < 0 || col >= 8) { return false; }
         return squares[row][col] == nullptr;
     }
-    bool hasopponent(int row, int col, Color myColor)
+
+    bool hasOpponent(int row, int col, Color myColor)
     {
 
         if (row < 0 || row >= 8 || col < 0 || col >= 8)
@@ -85,8 +77,8 @@ void initializeBoard()
         return (target->getcolor() != myColor);
     }
 };
-class Piece
-{
+
+class Piece{
 private:
     Pos pos;
 
@@ -104,60 +96,48 @@ public:
         return color;
     }
 };
+
 class Bishop : public Piece
 {
     public:
     Bishop():Piece(){}
-    bool isvalidmove(int fromRow, int fromCol, int toRow, int toCol, Board &board)
-    {
+
+    bool isvalidmove(int fromRow, int fromCol, int toRow, int toCol, Board &board){
         int rowdiff = fromRow - toRow;
         int coldiff = fromCol - toCol;
-        if (abs(rowdiff) != abs(coldiff))
-            return false;
-        if (fromRow == toRow || fromCol == toCol)
-            return false;
-        if (!board.isempty(toRow, toCol) && !board.hasopponent(toRow, toCol, color))
-            return false;
+        if (abs(rowdiff) != abs(coldiff)) { return false; }
+        if (fromRow == toRow || fromCol == toCol) { return false; }
+        if (!board.isEmpty(toRow, toCol) && !board.hasOpponent(toRow, toCol, color)) { return false; }
         int rowstep = (toRow > fromRow) ? 1 : -1;
         int colstep = (toCol > fromCol) ? 1 : -1;
         int currRow = fromRow + rowstep;
         int currCol = fromCol + colstep;
         while (currRow != toRow || currCol != toCol)
         {
-            if (!board.isempty(currRow, currCol) && !board.hasopponent(currRow, currCol, color))
-                return false;
+            if (!board.isEmpty(currRow, currCol) && !board.hasOpponent(currRow, currCol, color))
+                { return false; }
             currRow += rowstep;
             currCol += colstep;
         }
         return true;
     }
 };
-class Rook : public Piece
-{
-    bool isvalidmove(int fromRow, int fromCol, int toRow, int toCol, Board &board)
-    {
+
+class Rook : public Piece{
+    bool isvalidmove(int fromRow, int fromCol, int toRow, int toCol, Board &board){
         int rowstep, colstep;
         int rowdiff = fromRow - toRow;
         int coldiff = fromCol - toCol;
-        if (rowdiff == 0 && coldiff == 0)
-            return false;
-        if (rowdiff != 0 && coldiff != 0)
-            return false;
-        if (rowdiff != 0)
-            rowstep = (toRow > fromRow) ? 1 : -1;
-        else
-            rowstep = 0;
-
-        if (coldiff != 0)
-            colstep = (toCol > fromCol) ? 1 : -1;
-        else
-            colstep = 0;
+        if (rowdiff == 0 && coldiff == 0) { return false; }
+        if (rowdiff != 0 && coldiff != 0) { return false; }
+        if (rowdiff != 0) { rowstep = (toRow > fromRow) ? 1 : -1; }
+        else { rowstep = 0; }
+        if (coldiff != 0) { colstep = (toCol > fromCol) ? 1 : -1; }
+        else { colstep = 0; }
         int currRow = fromRow + rowstep;
         int currCol = fromCol + colstep;
-        while (currRow != toRow || currCol != toCol)
-        {
-            if (!board.isempty(currRow, currCol) && !board.hasopponent(currRow, currCol, color))
-                return false;
+        while (currRow != toRow || currCol != toCol){
+            if (!board.isEmpty(currRow, currCol) && !board.hasOpponent(currRow, currCol, color)) { return false; }
             currRow += rowstep;
             currCol += colstep;
         }
